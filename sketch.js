e46575b306e7962f69ae29a14949fed53409a0ff
nishant agrawal <nishant@Tanishas-MacBook-Air.local>
2020-10-08 15:53:41 +0530

    upload  files pj

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var car , wall  ;
+var speed , weight;
+
+function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50, 200, 50, 50);
+  car.shapeColor="white";
+
+  wall=createSprite(1200,200,60,height/2);
+  wall.shapeColor=color(80,80,80);
+
+  speed=random(55,90);
+  weight=random(450,1500);
+}
+
+
+function draw() {
+  background(0);  
+  car.velocityX = speed;
+
+  if(wall.x - car.x < (car.width + wall.width) / 2){
+    if(car.x>1190){
+      car.x=1190;
+    }
+
+    car.velocityX = 0;
+
+    var deformation = 0.5 * weight * speed * speed/22509;
+    
+    if(deformation > 180){
+      car.shapeColor = color(250,0,0);
+    }
+    if(deformation < 180 && deformation > 100){
+      car.shapeColor = color(230,230,0);
+    }
+
+    if(deformation < 100){
+      car.shapeColor = color(0,255,0);
+    }
+
+  }
+  
+drawSprites();
+textSize(20);
+fill("red");
+text("deformation:"+ deformation,1200,380);
+}
