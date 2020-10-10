302c0407bc68b029c5611afac7a3dd121b6ef91d
nishant agrawal <nishant@Tanishas-MacBook-Air.local>
2020-10-10 18:19:11 +0530

    upload  files pj

--- sketch.js
+++ sketch.js
@@ -35,11 +35,11 @@ function draw() {
     var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
     
     if(damage > 10){
-      bullet.shapeColor = color(255,0,0);
+      wall.shapeColor = color(255,0,0);
     }
 
     if(damage < 10){
-      bullet.shapeColor = color(0,255,0);
+      wall.shapeColor = color(0,255,0);
     }
 
   }
